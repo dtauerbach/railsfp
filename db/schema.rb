d544fd8bd4367864e9f4674a26d0ac5892d46eba
Dan Auerbach <dtauerbach@gmail.com>
2015-06-16 11:37:48 -0700

    WIP: add answers model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616180122) do
+ActiveRecord::Schema.define(version: 20150616182019) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "answer_json"
+    t.integer  "uuid"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "question_text"
