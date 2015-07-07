a13654c468d987be750c897d81aa379c5ae9b670
Dan Auerbach <dtauerbach@gmail.com>
2015-07-07 15:20:04 -0700

    Tweaks to get d3plus box plot displaying with fake data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,20 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require d3
+//= require d3plus
 //= require_tree .
+
+var data = [
+    {"year": 1992, "name":"alpha", "value": 34},
+    {"year": 1992, "name":"alpha2", "value": 65},
+    {"year": 1992, "name":"beta", "value": 10},
+    {"year": 1992, "name":"beta2", "value": 38},
+    {"year": 1992, "name":"gamma", "value": 10},
+    {"year": 1992, "name":"gamma2", "value": 34},
+    {"year": 1992, "name":"delta", "value": 43},
+    {"year": 1992, "name":"delta2", "value": 35}
+]
 
 submit_answer = function(answer_text) {
     $('input[name="answer_json"]').val(answer_text);
@@ -29,7 +42,18 @@ ready = function() {
     $(".submit-button").on("click", function(){
         submit_answer($('input[name="answer"]').val());
     });
+
+    var visualization = d3plus.viz()
+        .container("#viz")
+        .data(data)
+        .type("box")
+        .id("name")
+        .x("value")
+        .y("year", {"scale": "discrete"})
+        .time("year")
+        .draw()
 }
+
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
