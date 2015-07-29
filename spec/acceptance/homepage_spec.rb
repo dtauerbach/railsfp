e69014d3faac397f57087e6448e22673fdaa0999
Dan Auerbach <dtauerbach@gmail.com>
2015-07-29 14:33:11 -0700

    Make homepage spec pass; jigger grayscale manually again

--- spec/acceptance/homepage_spec.rb
+++ spec/acceptance/homepage_spec.rb
@@ -4,10 +4,10 @@ describe 'example' do
   it 'trivial test' do
     expect(1).to eq(1)
   end
-  
+
   it 'homepage loading test', js: true, driver: :selenium do
     visit '/'
-    expect(page).to have_content("asdff")
+    expect(page).to have_content("What are you worth")
   end
 
 end
